7dbd12fd9e3537624c483e07b921b251172f3ce3
Masoom-Wahid <masoom3892@gmail.com>
2024-03-15 13:12:30 +0430

    Added Binary Heap

---
+++ ds/binary_heap.py
@@ -0,0 +1,15 @@
+import heapq
+min_this = [1,2,3,4,5]
+max_this = [3,4,5,6,7,8]
+
+# for max heap
+heapq.heapify(min_this)
+# for min heap
+heapq._heapify_max(max_this)
+
+
+first_smallest_number = heapq.heappop(min_this)
+first_largest_number = heapq.nlargest(1,max_this)
+
+print(first_smallest_number)
+print(first_largest_number)
